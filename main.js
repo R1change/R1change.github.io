bbfefe8f8840312a6a0160bb74dac204fc6bd200
R1change <136600258+R1change@users.noreply.github.com>
2023-06-15 19:21:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,76 +1,73 @@
-var rec={}
-var n=0
+var rec = {}
+var n = 0
 var xx //最大人数1开始需要赋值
 
-function init_rec(z){
-    if(z>0&&z<5){
-        xx=z
-        for(i=1;i<=z;++i){
-            rec[i]={}
-            rec[i].stay=0
-            rec[i][0]={}
-            rec[i][0].boxNum=0
+function init_rec(z) {
+    if (z > 0 && z < 5) {
+        n = 0
+        xx = z
+        for (i = 1; i <= z; ++i) {
+            rec[i] = {}
+            // rec[i].stay=0
+
+            rec[i].boxNum = 0
         }
-        //console.log(JSON.stringify(rec))
+        console.log(JSON.stringify(rec))
     }
     else console.log("人数不正确")
 
 }
 
-function start(x,y){
+function start(x, y) {
     //判断是否所有人完成
-    var IsDone=1
-    for(i=1;i<=xx;++i){
+    var IsDone = 1
+    for (i = 1; i <= xx; ++i) {
 
-        if(typeof rec[i][n]=="undefined"){IsDone=0}
+        if (typeof rec[i] == "undefined") { IsDone = 0 }
     }
-    if(IsDone==1)++n
-    
-    
+    if (IsDone == 1) ++n
+
+
     //updata content boxNum done stay NO DICE
-    function updata(){
+    function updata() {
+        rec[x].done = 0 //是否到达终点
         //防止溢出
-        if(boxNum>dfw.length)boxNum=dfw.length
-        if(boxNum<1)boxNum=1
-        rec[x][n].boxNum=boxNum
+        if (boxNum > dfw.length){
+            boxNum = dfw.length
+            rec[x].done = 1
+        } 
+        if (boxNum < 1) boxNum = 1
+        rec[x].boxNum = boxNum
+        if (typeof rec[x].content == "undefined") rec[x].content = dfw[boxNum][2]
+        // else rec[x].content += dfw[boxNum][2]
+    }
+    var dice = Math.ceil(Math.random() * 6)
+    // dice = 4
+    if (y != 0) dice = y
 
-        if(typeof rec[x][n].content=="undefined")rec[x][n].content=dfw[boxNum][2]
-        else rec[x][n].content+=dfw[boxNum][2]
-        if(dfw[boxNum][0]=="stay"){rec[x].stay+=dfw[boxNum][1]}
-        rec[x][n].done=0
-    }
-//判断停留
-    if(rec[x].stay!==0){
-        --rec[x].stay
-        rec[x][n]=JSON.parse(JSON.stringify(rec[x][n-1]))
-        rec[x][n].dice=0
-        rec[x][n].content=`停留 ${rec[x].stay+1}格`
-    }
-//正常
-    else{
-        var dice=Math.ceil(Math.random()*6)
-        if(y!=0)dice=y
-        var boxNum=rec[x][n-1].boxNum+dice
-        //if(boxNum>dfw.length)boxNum=dfw.length
-        rec[x][n]={}
-        rec[x][n].dice=dice
+    var boxNum = rec[x].boxNum + dice
+    // var boxNum = rec[x].boxNum + 3
+    // console.log("boxNum======",boxNum);
+    rec[x] = {}
+    rec[x].dice = dice
+    updata()
+    if (dfw[boxNum][0] == "move" && dfw[boxNum][1] != 0) {
+        boxNum += dfw[boxNum][1]
+        // console.log(dfw[boxNum])
+        // rec[x].content += `---> ${dfw[boxNum][2]} `
         updata()
-        while(dfw[boxNum][0]=="move"&&dfw[boxNum][1]!=0){
-            boxNum+=dfw[boxNum][1]
-            rec[x][n].content+=` 再跳${dfw[boxNum][1]}格 `
-
-            updata()
-        }
     }
-    
-    console.log(rec)
-    return rec[x][n]
+    else if(dfw[boxNum][0] == "modo" && dfw[boxNum][1] != 0){
+        boxNum += dfw[boxNum][1]
+        rec[x].content += `---> ${dfw[boxNum][2]} `
+        updata()
+    }
+    else if(dfw[boxNum][0] == "fly" && dfw[boxNum][1] != 0){
+        boxNum = dfw[boxNum][1]
+        updata()
+    }
+    console.log(rec[x]);
+    return rec[x]
 }
-//调试
 
-//init_rec(2)
-//start(1,4)
-//start(2,0)
-//start(1,0)
-//start(2,0)
 
